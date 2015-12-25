56a24e63c797416b281cad10bcb42b48bfce0f48
kasselTrankos <alvaro.touzon@gmail.com>
2015-12-25 19:27:52 +0100

    clean files again

--- src/components/twitter/Account.js
+++ src/components/twitter/Account.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Form from 'react-router-form'
 
 const Account = React.createClass({
   render(){
@@ -6,6 +7,18 @@ const Account = React.createClass({
     return (
       <li className="list-group-item">{account.account}</li>
     );
+  }
+});
+const NewAccountForm = React.createClass({
+  render(){
+    return (
+      <Form className="form-inline" method="POST" to="/twitter/user_timeline">
+      <div className="form-group">
+        <label htmlFor="account">Twitter User name</label>
+        <input type="text" className="form-control" id="account" placeholder="Twitter User name" />
+      </div>
+      <button type="submit" className="btn btn-default">Guardar</button>
+    </Form>)
   }
 });
 
